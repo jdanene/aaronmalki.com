b0dfaaf3897f3e859e40c1ecedca9a06e26ec8cc
Jidé Anene <dominicanene@gmail.com>
2020-10-22 11:35:10 -0700

    placeholder

---
+++ src/views/ContactUsPage/GoogleMaps/NavigateToGoogleMapButton.js
@@ -0,0 +1,118 @@
+import React, {useCallback, useState, useContext, useEffect} from 'react'
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import {
+    withStyles,
+    withWidth,
+} from "@material-ui/core";
+
+import {AppContext} from "../../context";
+
+import {StyledText} from "../../components/Text";
+import {colorScheme} from "../../constants";
+import {FaDirections} from "react-icons/fa";
+import Tooltip from '@material-ui/core/Tooltip';
+
+
+const styles = theme => ({
+    container: {
+        position: "relative",
+        left: 5,
+        top: 5,
+        maxWidth: 300,
+        maxHeight: 200
+    },
+    directionButton: {
+        padding:0,
+        display:'flex',
+        border: '1px solid yellow',
+        color: colorScheme.general.light_blue0,
+        "&:hover": {
+            //you want this to be the same as the backgroundColor above
+            backgroundColor: "transparent", color: colorScheme.general.other_blue
+        }
+    },
+    directionIcon: {
+        color: colorScheme.general.light_blue0,
+        "&:hover": {
+            //you want this to be the same as the backgroundColor above
+            color: colorScheme.general.other_blue
+        }
+    },
+    largerMapButton: {
+        color: colorScheme.general.light_blue0,
+        "&:hover": {
+            //you want this to be the same as the backgroundColor above
+            color: colorScheme.general.other_blue
+        }
+    }
+});
+
+/*
+*             address: {
+                line1: "891 Beech Street",
+                line2: "San Francisco CA 94109",
+                position:
+                    {
+                        lat: 37.806279,
+                        lng: -122.423516
+                    }
+            },
+            phoneNumber: {
+                dash: "(909) 771-6881",
+                dot: "909.771.6881"
+            },
+            email: "aaronmalki@malki.com"
+        };
+*
+* */
+const NavigateToGoogleMapButton = ({classes, theme, width, center, zoom}) => {
+    const {address, phoneNumber, email} = useContext(AppContext);
+
+
+    return (
+        <Card className={classes.container}>
+
+
+            <div style={{display: 'flex', justifyContent: 'space-between'}}>
+                <CardContent>
+                    <StyledText gutterBottom variant="h5" component="h5"
+                                style={{textAlign: 'left', fontWeight: 'bold', fontSize: 16}}>
+                        {address.line1}
+                    </StyledText>
+                    <StyledText variant="body2" color="textSecondary" component="p" style={{textAlign: 'left'}}>
+                        {address.line1} <br/>
+                        {address.line2} <br/>
+                    </StyledText>
+                </CardContent>
+
+                <Button size="small" disableRipple className={classes.directionButton}>
+                    <Tooltip title="Get directions on Google Map">
+
+                        <div style={{flexDirection: 'column', display: 'flex'}}>
+
+                            <div aria-label="direction">
+                                <FaDirections size={24}/>
+                            </div>
+
+
+                            <StyledText>
+                                Directions
+                            </StyledText>
+                        </div>
+                    </Tooltip>
+
+                </Button>
+            </div>
+
+            <Button style={{float: 'left', marginLeft: 3}} size="small" className={classes.largerMapButton}>
+                View Larger Map
+            </Button>
+
+        </Card>
+
+    )
+};
+
+export default withWidth()(withStyles(styles, {withTheme: true})(NavigateToGoogleMapButton));
---
+++ src/views/ContactUsPage/GoogleMaps/ShowGoogleMap.js
@@ -0,0 +1,122 @@
+import React, {useCallback, useState, useContext, useEffect} from 'react'
+import PropTypes from 'prop-types'
+import {GoogleMap, OverlayView, Marker, LoadScript, InfoBox, Data, InfoWindow} from '@react-google-maps/api'
+import {firebaseConfig} from "../../App";
+import {StyledText} from "../../components/Text";
+import {colorScheme} from "../../constants";
+import {AppContext} from "../../context";
+import NavigateToGoogleMapButton from "./NavigateToGoogleMapButton";
+
+const centerOverlayView = (width, height) => ({
+    x: -(width / 2),
+    y: -(height / 2),
+});
+
+const mapContainerStyle = {
+    height: "400px",
+    width: "800px",
+};
+
+
+const ShowGoogleMap = ({styles}) => {
+    const {address,phoneNumber,email} = useContext(AppContext);
+
+    const [overlayPane, setOverlayPane] = React.useState(
+        OverlayView.FLOAT_PANE
+    );
+
+    const clickHandler = React.useCallback(() => {
+        alert('You clicked overlay view')
+    }, []);
+
+    const loadCallback = React.useCallback((e) => {
+        console.log('OverlayView onLoad: ', e)
+    }, []);
+    const unmountCallback = React.useCallback((e) => {
+        console.log('OverlayView onUnmount', e)
+    }, []);
+
+    // options for InfoWindow or InfoBox not sure
+    const options = {closeBoxURL: '', enableEventPropagation: true, pixelOffset: 5};
+
+
+    return (
+        <LoadScript
+            googleMapsApiKey={firebaseConfig.apiKey}
+        >
+            <GoogleMap
+                id='traffic-example'
+                mapContainerStyle={mapContainerStyle}
+                zoom={13}
+                center={address.position}
+            >
+                    <NavigateToGoogleMapButton/>
+
+
+                <OverlayView
+                    position={address.position}
+                    mapPaneName={overlayPane}
+                    onLoad={loadCallback}
+                    onUnmount={unmountCallback}
+                    getPixelPositionOffset={centerOverlayView}
+                >
+                    <StyledText
+                        style={{
+                            color: colorScheme.general.red,
+                            fontSize: '10px',
+                            position: 'relative',
+                            top: -20,
+                            left: 70,
+                            paddingLeft:'2px',
+                            fontWeight:'bold',
+                            border: '1px solid yellow',
+                            textAlign:'left'
+                        }}
+                        onClick={clickHandler}
+                    >
+                        {address.line1} <br/>
+                        {address.line2}
+                    </StyledText>
+                </OverlayView>
+
+
+                {/*https://react-google-maps-api-docs.netlify.app/#marker*/}
+                <Marker position={address.position} />
+
+                {/*
+                <InfoWindow
+                    position={address.position}
+                    options={options}
+                >
+                    <div style={{backgroundColor: 'yellow', opacity: 0.75, padding: 12, height: '70px'}}>
+                        <div style={{fontSize: 16, fontColor: `#08233B`}}>
+                            Hello, World!
+                        </div>
+                    </div>
+                </InfoWindow>
+                */}
+            </GoogleMap>
+        </LoadScript>
+
+    )
+}
+// formatted_address, long_name
+//long=x
+//lat=y
+/*
+static
+relative
+fixed
+absolute
+sticky
+*/
+
+const ExampleOverlayViewPropTypes = {
+    styles: PropTypes.shape({
+        container: PropTypes.object.isRequired,
+    }).isRequired,
+};
+
+ShowGoogleMap.propTypes = ExampleOverlayViewPropTypes;
+
+export default React.memo(ShowGoogleMap)
